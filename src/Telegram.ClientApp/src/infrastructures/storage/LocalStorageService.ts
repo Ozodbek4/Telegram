0fd981812e01eff64c30d1139ca3470a77ca5028
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2024-09-17 16:22:35 +0500

    added local storage

--- src/Telegram.ClientApp/src/infrastructures/storage/LocalStorageService.ts
+++ src/Telegram.ClientApp/src/infrastructures/storage/LocalStorageService.ts
@@ -1,3 +1,18 @@
 export class LocalStorageService{
-    
+    public get<T>(key: string): T | null{
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) as T : null;
+    }
+
+    public set<T>(key: string, value: T): void {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    public remove(key: string): void {
+        localStorage.removeItem(key);
+    }
+
+    public clear(key: string): void{
+        localStorage.clear();
+    }
 }

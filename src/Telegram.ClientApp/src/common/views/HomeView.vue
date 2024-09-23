2e144066e1ed58133a3b43f751f996ee3485f8d9
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2024-09-23 15:46:53 +0500

    added login page

--- src/Telegram.ClientApp/src/common/views/HomeView.vue
+++ src/Telegram.ClientApp/src/common/views/HomeView.vue
@@ -1,15 +1,8 @@
-<template>
-    <form @sumbit.prevent = "signUp">
-        <button type="submit">SignIn</button>
-    </form>
-</template>
-
 <script setup lang="ts">
-import router from '@/infrastructures/router';
-import SignUp from '@/modules/components/SignUp.vue';
-
-const signUp = () => {
-    router.push({name: 'Register'});
-}
-
+import SignIn from '@/modules/components/SignIn.vue';
+import BaseView from '../components/BaseView.vue'
 </script>
+
+<template>
+    <BaseView></BaseView>
+</template>

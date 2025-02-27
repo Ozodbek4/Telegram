2e3e7d10b6023e96ff3159375ecbb818679d4888
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2025-02-27 21:45:46 +0500

    fixed time format

--- src/Telegram.ClientApp/src/modules/components/MessageItem.vue
+++ src/Telegram.ClientApp/src/modules/components/MessageItem.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="max-w-xs p-4 rounded-lg shadow-xl" :class="isMe ? 'justify-end bg-sky-200 ml-auto' : 'justify-start bg-white mr-auto'">
+    <div class="max-w-xs p-3 rounded-lg shadow-xl" :class="isMe ? 'justify-end bg-sky-200 ml-auto' : 'justify-start bg-white mr-auto'">
         <p class="text-sm">{{ message.body }}</p>
+        
+        <!-- <div v-if="isMe" class="flex justify-end text-gray-500 text-xs gap-2">
+            <span>{{ formattedDate }}</span><span>{{ message.isSeen ? 'seen' : 'sended' }}</span>
+        </div> -->
+        <div v-if="isMe" class="flex justify-end text-gray-500 text-xs">
+            <span class="ml-1">{{ formattedDate }}</span>
+        </div>
         <div v-if="isMe" class="flex justify-end text-gray-500 text-xs">
             <span class="ml-1">{{ message.isSeen ? 'seen' : 'sended' }}</span>
         </div>
@@ -16,5 +23,18 @@ import { computed } from 'vue';
 const me = LocalStorageService.get<User>('me');
 const proms = defineProps<{ message: Message }>();
 const isMe = computed(() => proms.message.senderId === me?.id);
-// const receiver = computed(() => isMe.value ? proms.message.receiver : proms.message.sender);
+const formattedDate = computed(() => {
+    const date = new Date(proms.message.createdAt);
+    const time = date.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    });
+    const datePart = date.toLocaleDateString("en-US", {
+        month: "2-digit",
+        day: "2-digit",
+        year: "2-digit"
+    });
+    return `${time} ${datePart}`;
+});
 </script>
--- src/Telegram.ClientApp/src/modules/models/Message.ts
+++ src/Telegram.ClientApp/src/modules/models/Message.ts
@@ -1,15 +1,16 @@
 import type { User } from "./User";
 
-export interface Message{
+export interface Message {
     id: number;
 
     senderId: number;
     sender: User;
-    
+
     receiverId: number;
     receiver: User;
 
     chatRoomId: number;
     body: string;
     isSeen: boolean;
+    createdAt: string;
 }
